da28db8248b1285c8430fd7a45a9bdb1e94a1e73
aryawiratama18 <NAwiratama@gmail.com>
2021-05-28 06:35:16 +0700

    Delete comments

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -4,7 +4,6 @@ const swRegister = async () => {
   if ('serviceWorker' in navigator) {
     await runtime.register();
   }
-  // console.log('Service worker not supported in this browser');
 };
 
 export default swRegister;
